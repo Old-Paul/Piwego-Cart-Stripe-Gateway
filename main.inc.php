3910865ad7f62f151084f2277a0b610f2e060081
Old-Paul <102179055+Old-Paul@users.noreply.github.com>
2026-07-09 12:17:15 -0400

    Switch license to GPLv2 to match Piwigo core (required for PEM eligibility)

--- main.inc.php
+++ main.inc.php
@@ -5,7 +5,7 @@ Version: 1.1.0
 Description: Stripe payment gateway for the Cart plugin. Requires the "cart" plugin to be active.
 Author: Webgoodies
 Has Settings: true
-License: GPL3
+License: GPL2
 */
 
 defined('PHPWG_ROOT_PATH') or die('Hacking attempt!');
